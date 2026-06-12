f587be966db76fe4b270110444ba5d8dbeebcbc3
jnetoatbliss <jneto@blissapplications.com>
2026-06-12 15:34:23 -0300

    Initial Commit

---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by Zé Neto on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
